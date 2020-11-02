ab031d3505a29b568c311abf0dc342d0e6f63fa7
Moin-11 <Moin11.dev@gmail.com>
2020-11-02 16:08:54 +0530

    implement projects slice

---
+++ src/functional/projects.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let lastID;
+const projectsSlice = createSlice({
+  name: "Projects",
+  initialState: [],
+  reducers: {
+    addProject: (projects, action) => {
+      projects.push({
+        id: ++lastID,
+        name: action.payload.name,
+      });
+    },
+
+    discardProject: (projects, action) => {
+      return projects.filter((project) => projects.id !== action.payload.id);
+    },
+
+    completeProject: (projects, action) => {
+      const index = projects.findIndex(
+        (project) => project.id === action.payload.id
+      );
+      projects[index].completed = true;
+    },
+  },
+});
